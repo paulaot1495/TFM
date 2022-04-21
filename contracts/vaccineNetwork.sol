7d0a1ae3bc329d8c177f105e78de6648b1643e79
Paula <paulaoterocolmenar@gmail.com>
2022-04-21 04:18:07 +0200

    vaccineNetwork sol

--- contracts/vaccineNetwork.sol
+++ contracts/vaccineNetwork.sol
@@ -22,6 +22,7 @@ contract VaccineNetwork is LaboratoryRole, CarrierRole, VaccineCenterRole {
     //Add Laboratory role to an account. Only admin account can use it.
     function addLaboratory(address account) public {
         require(!laboratory, "Ya hay un laboratorio creado, eliminalo antes");
+        require(roles[account] == Rol.None, "Esta cuenta ya esta asignada a un rol");
         users[Rol.Laboratory] = account;
         roles[account] = Rol.Laboratory;
         laboratory = true;
@@ -30,8 +31,9 @@ contract VaccineNetwork is LaboratoryRole, CarrierRole, VaccineCenterRole {
 
       //Remove laboratory role of an account. Only admin account can use it. 
     function removeLaboratory(address account) public {
-        require(users[Rol.Laboratory] != empty, "Esta cuenta no tiene rol de laboratorio");
+        require(users[Rol.Laboratory] == account, "Esta cuenta no tiene rol de laboratorio");
         roles[account] = Rol.None;
+        users[Rol.Laboratory] = empty;
         laboratory = false;
         _removeLaboratory(account);
     }
@@ -39,16 +41,18 @@ contract VaccineNetwork is LaboratoryRole, CarrierRole, VaccineCenterRole {
     //Add VaccineCenter role to an account. Only admin account can use it.
     function addVaccineCenter(address account) public {
         require(!center, "Ya hay un responsable del centro de vacunacion creado");
+        require(roles[account] == Rol.None, "Esta cuenta ya esta asignada a un rol");
         users[Rol.VaccineCenter] = account;
         roles[account] = Rol.VaccineCenter;
-        laboratory = true;
+        center = true;
         _addLaboratory(account);
     }
 
     //Remove VaccineCenter role of an account. Only admin account can use it. 
     function removeVaccineCenter(address account) public {
-        require(users[Rol.VaccineCenter] != empty, "Esta cuenta no tiene rol de centro");
+        require(users[Rol.VaccineCenter] == account, "Esta cuenta no tiene rol de centro");
         roles[account] = Rol.None;
+        users[Rol.VaccineCenter] = empty;
         center = false;
         _removeVaccineCenter(account);
     }
@@ -56,22 +60,28 @@ contract VaccineNetwork is LaboratoryRole, CarrierRole, VaccineCenterRole {
     //Add Carrier role to an account. Only admin account can use it.
     function addCarrier(address account) public {
         require(!carrier, "Ya hay un transportista creado, eliminalo antes");
+        require(roles[account] == Rol.None, "Esta cuenta ya esta asignada a un rol");
         users[Rol.Carrier] = account;
         roles[account] = Rol.Carrier;
-        laboratory = true;
+        carrier = true;
         _addCarrier(account);
     }
 
     //Remove Carrier role of an account. Only admin account can use it. 
     function removeCarrier(address account) public {
-        require(users[Rol.Carrier] != empty, "Esta cuenta no tiene rol transportista");
+        require(users[Rol.Carrier] == account, "Esta cuenta no tiene rol transportista");
         roles[account] = Rol.None;
+        users[Rol.Carrier] = 0x0000000000000000000000000000000000000000;
         carrier = false;
         _removeCarrier(account);
     }
 
     function getAccountRole (address _address) public view returns (Rol) {
         return roles[_address];
+    }
+
+    function getRoleAccount (Rol _rol) public view returns (address) {
+        return users[_rol];
     }
 
 }
